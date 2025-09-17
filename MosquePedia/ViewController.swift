5b51f5af87e445770e3d1dbad9fdf9bcda3b1d13
mai <maikhaled41@icloud.com>
2025-09-17 22:45:52 +0300

    Initial Commit

---
+++ MosquePedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MosquePedia
+//
+//  Created by mai K on 17/09/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
